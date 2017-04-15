66ea93211e74063b099d2c3b6820a7d437725709
MINTY1st <minty0305@gmail.com>
2017-04-15 20:52:20 +0900

    index.html更新とその他
    表題通り

---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    var open = false;
+    $('.circle-bg-t').on('click', function () {
+        if (open === false) {
+            $(this).animate({
+                height: '+=10px',
+                width: '+=10px'
+            }, 300);
+
+            $('.outer-icons-t').animate({
+                opacity: 1
+            }, 1000);
+
+
+            open = true;
+        } else {
+            $(this).animate({
+                height: '-=10px',
+                width: '-=10px'
+            }, 200);
+
+            $('.outer-icons-t').animate({
+                opacity: 0
+            }, 300);
+
+
+
+            open = false;
+        }
+
+    });
+});
+$(document).ready(function () {
+    var open = false;
+    $('.circle-bg-w').on('click', function () {
+        if (open === false) {
+            $(this).animate({
+                height: '+=10px',
+                width: '+=10px'
+            }, 300);
+
+            $('.outer-icons-w').animate({
+                opacity: 1
+            }, 1000);
+
+
+            open = true;
+        } else {
+            $(this).animate({
+                height: '-=10px',
+                width: '-=10px'
+            }, 200);
+
+            $('.outer-icons-w').animate({
+                opacity: 0
+            }, 300);
+
+
+
+            open = false;
+        }
+    });
+
+});
